9e9a9810bd8f9cfdaf8f1f50c3367639796ca78e
Submissions-byte <ST10495654@vcconnect.edu.za>
2025-06-26 23:56:53 +0200

    Update LoginTest.java

--- POE/src/test/java/com/mycompany/poe/LoginTest.java
+++ POE/src/test/java/com/mycompany/poe/LoginTest.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.poe;
 
+import javax.swing.JOptionPane;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,60 +20,97 @@ public class LoginTest {
 
     }
 
+    
     @Test
-    public void testUserName() {
-        assertTrue(user.checkUserName("kyl_1"), "True");
-        assertFalse(user.checkUserName("kyle!!!!!!!"), "False");
-    }
-
-    @Test
-    public void testPassword() {
-        assertTrue(user.checkPasswordComplexity("Ch&&sec@ke99"), "True");
-        assertFalse(user.checkPasswordComplexity("password"), "False");
-    }
-
-    @Test
-    public void testPhone() {
-        assertTrue(user.checkCellPhoneNumber("+27838968976"), "True");  //boolean testing
-        assertFalse(user.checkCellPhoneNumber("08966553"), "False");
-
-        Login user1 = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
-        assertEquals(user1.phoneMessaging(), "Cell phone number successfully added.", "Cell phone number successfully captured.");
-
-        user1 = new Login("kyl_1", "Ch&&sec@ke99", "08966553", "<user first name>", "<user last name>");
-        assertEquals("Cell phone number incorrectly formatted or does not contain international code.", user1.phoneMessaging(), "Cell number is incrorrectly formatted or does not contain an international code, please correct the number and try again.");
-    }
-
-    @Test
-    public void testLogin() {
-        assertFalse(user.loginUser("different", "different"), "False");  //boolean testing
-        Login user1 = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
-
-        assertTrue(user1.loginUser("Ch&&sec@ke99", "kyl_1"), "True");
-
-        user1 = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
+    public void testUsernameSuccessfullyFormattedMessaging() {
+ Login user1 = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
         user1.registerUser();
         assertEquals("Welcome <user first name> ,<user last name> it is great to see you again.", user1.returnLoginStatus("Ch&&sec@ke99", "kyl_1"), "Welcome <user first name> ,<user last name> it is great to see you.");
-        assertEquals("Username or password incorrect, please try again.", user1.returnLoginStatus("p", "u"));
+    }//compares login messaging to expected messaging
+    
+    @Test
+    public void testUsernameUnsuccessfullyFormattedMessaging() {
+         user = new Login("kyle!!!!!!!", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
+        //assertEquals("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.",user.registerUser());
+        assertEquals("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.", user.registerUser(),"Username is not correctly formatted, please ensure that your username is correctly contains an underscore and is no more than five characters in length.");
+    }//corrections due to differences in strings between test cases and suggestions above
+    //original, with test case suggested string in comments
+
+    @Test
+    public void testPasswordComplexityMetMessaging() {
+         user = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
+        
+
+        assertEquals("Password successfully captured.", user.registerUser().substring(user.registerUser().indexOf("Password"), user.registerUser().indexOf(".", user.registerUser().indexOf("Password"), user.registerUser().length()) + 1));
+    }//retrieves substring containing relevant line of string to compare
+
+    @Test
+    public void testPasswordComplexityNotMetMessaging() {
+         user = new Login("kyl_1", "password", "+27838968976", "<user first name>", "<user last name>");
+        
+        //assertEquals("Password is not correctly formatted, please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.",user.registerUser());
+        assertEquals("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.", user.registerUser());
+    }
+
+    @Test
+    public void testPhoneCorrectlyFormatted() {
+         user = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
+        assertEquals(user.phoneMessaging(), "Cell phone number successfully added.", "Cell phone number successfully captured.");
 
     }
 
     @Test
-    public void testRegister() {
-        Login user1 = new Login("kyle!!!!!!!", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
-        assertEquals("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.", user1.registerUser(), "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.");
+    public void testPhoneIncorrectlyFormatted() {
+         user = new Login("kyl_1", "Ch&&sec@ke99", "08966553", "<user first name>", "<user last name>");
+        assertEquals("Cell phone number incorrectly formatted or does not contain international code.", user.phoneMessaging(), "Cell number is incrorrectly formatted or does not contain an international code, please correct the number and try again.");
 
-        user1 = new Login("kyl_1", "password", "+27838968976", "<user first name>", "<user last name>");
-        assertEquals("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.", user1.registerUser(), "Password is not correctly formatted, please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.");
+    }
 
-        user1 = new Login("kyle!!!!!!!", "password", "+27838968976", "<user first name>", "<user last name>");
-        assertTrue(user1.registerUser().contains("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character."), "Password is not correctly formatted, please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.");
-        assertTrue(user1.registerUser().contains("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length."), "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.");
+    @Test
+    public void testLoginSuccessful() {
+        user = new Login("2", "1", "08966553", "<user first name>", "<user last name>");
+        assertTrue(user.loginUser("1", "2"));
+    }
 
-        user1 = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
-        assertTrue(user1.registerUser().contains("Password successfully captured."), "Password successfully captured.");
-        assertTrue(user1.registerUser().contains("Username successfully captured."), "Welcome <user first name> ,<user last name> it is great to see you.");
+    @Test
+    public void testLoginFailed() {
+        user = new Login("1", "2", "08966553", "<user first name>", "<user last name>");
+        
+        assertFalse(user.loginUser("3", "4"));
+    }
 
+    @Test
+    public void testUserNameCorrectlyFormatted() {
+       user = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
+        assertTrue(user.checkUserName("kyl_1"));
+    }
+
+    @Test
+    public void testUserNameIncorrectlyFormatted() {
+        user = new Login("kyle!!!!!!!", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
+        assertFalse(user.checkUserName("kyle!!!!!!!"));
+    }
+
+    @Test
+    public void testPasswordMeetsComplexityRequirements() {
+        user = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
+        assertTrue(user.checkPasswordComplexity("Ch&&ck@e99"));
+    }
+
+    @Test
+    public void testPasswordDoesNotMeetComplexityRequirements() {
+        user = new Login("kyl_1", "Ch&&sec@ke99", "+27838968976", "<user first name>", "<user last name>");
+        assertFalse(user.checkPasswordComplexity("password"));
+    }
+
+    @Test
+    public void testPhoneCorrectlyFormattedBoolean() {
+        assertTrue(user.checkCellPhoneNumber("+27838968976"), "True");
+    }
+
+    @Test
+    public void testPhoneIncorrectlyFormattedBoolean() {
+        assertFalse(user.checkCellPhoneNumber("08966553"), "False");
     }
 
 }
